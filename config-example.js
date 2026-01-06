a438240363ffca96db5096468eaab6e8d2bda882
Jugachi <contact@jugachi.com>
2026-01-06 18:09:03 +0100

    Add files via upload

---
+++ config-example.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    TWITCH_CLIENT_ID: 'TWITCH_CLIENT_ID',
+    TWITCH_OAUTH_TOKEN: 'TWITCH_ACCESS_TOKEN', // Scope: moderator:read:followers
+    BROADCASTER_NAME: 'Jugachi',
+    DISPLAY_TIME: 8000,
+    ANIMATION_IN: 'left',
+    ANIMATION_OUT: 'top',
+    SOUND_FILE: 'assets/notification.opus', // Place your sound file in the assets folder
+    SOUND_VOLUME: 1.0
+};
